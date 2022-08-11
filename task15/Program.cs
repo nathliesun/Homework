a21b89a35c937555a7580ceff91c5689c9361e57
Natalia <aniskinan@internet.ru>
2022-08-11 20:51:51 +0500

    добавила 3 задачу

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите  день недели цифрой от 1 до 7");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 5)
+   Console.WriteLine(" это будний день");
+else if (number <= 7)
+    Console.WriteLine(" это выходной");
+else  
+    Console.WriteLine(" это не день недели, введите число от 1 до 7");
