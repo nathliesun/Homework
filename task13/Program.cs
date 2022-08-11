cba832fe193ed40457c6443820427a10388e994c
Natalia <aniskinan@internet.ru>
2022-08-11 19:39:48 +0500

    коммит2

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите  число");
+
+string str_number = Console.ReadLine();
+
+if (str_number.Length < 3)
+   Console.WriteLine(" третьей цифры нет");
+else 
+{
+    char digit= str_number[2];
+    Console.WriteLine(" третья цифра");
+    System.Console.WriteLine(digit);
+}
