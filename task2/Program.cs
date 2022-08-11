e889f2c232b0e6319d892aade703dc9da560f46c
Natalia <aniskinan@internet.ru>
2022-08-11 19:39:07 +0500

    коммит

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string str_number = number.ToString();
+char digit= str_number[1];
+Console.WriteLine("вторая цифра");
+System.Console.WriteLine (digit);
