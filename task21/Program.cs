491f402a5e8ce3319e143f188cf09a3f5d2b6642
Natalia <aniskinan@internet.ru>
2022-08-12 17:25:09 +0500

    добавила задачу 21

---
+++ task21/Program.cs
@@ -0,0 +1,8 @@
+﻿double distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+   double result = Math.Pow (Math.Pow ((x2-x1), 2) + Math.Pow ((y2-y1), 2) + Math.Pow ((z2-z1), 2), 0.5);
+return result;
+}
+double dis = distance(2,8,6,9,7,8);
+
+Console.WriteLine(dis);
