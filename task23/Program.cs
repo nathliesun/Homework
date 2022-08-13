2ecc479cfe9b9dbba1c960a52fa76225b6328f1f
Natalia <aniskinan@internet.ru>
2022-08-13 23:29:24 +0500

    добавила задачу 23

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите число");
+double number = Convert.ToInt32(Console.ReadLine());
+
+for (double i =1; i <= number; i++)
+{
+double result = Math.Pow (i, 3);
+Console.WriteLine(result);
+}
