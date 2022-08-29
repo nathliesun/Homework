c24ebcf6cd5afa024fafc64db16272163829d714
Natalia <aniskinan@internet.ru>
2022-08-29 20:42:25 +0500

    дз 41, 43

---
+++ task41/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("введите 7 чисел");
+int[] ReadArray(int size)
+{
+ int[] result = new int[size];
+    
+    for(int i = 0; i<size; i++)
+    {
+        result[i]=Convert.ToInt32(Console.ReadLine()); //каждому элементу массива присваиваем введенное по очереди число
+    }
+    return result;   
+}
+
+
+
+void PrintArray(int[] a) // вывод массива
+{
+    Console.Write("[");
+    Console.Write(a[0]);
+    for(int i=1;i<a.Length;i++)
+    {
+        Console.Write(", ");
+        Console.Write(a[i]);
+    }
+    Console.WriteLine("]");
+}
+
+
+int FindNumber(int[] array)
+{
+int number=0;
+for(int i=0;i<array.Length;i++)
+{
+    if(array[i] > 0)
+        number++;
+} 
+return number;  
+}
+int[] array = ReadArray(7);
+PrintArray(array);
+int number = FindNumber(array);
+Console.Write("Чиcел больше нуля введено ");
+Console.WriteLine(number);
+
---
+++ task43/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Для уравнения y = k1 * x + b1, y = k2 * x + b2 введите значения b1, k1, b2 и k2 ");
+Console.WriteLine(" b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+double[] FindPoint(double b1, double k1, double b2, double k2)
+{
+
+ double[] koord = new double[2];
+ koord[0]=(b2-b1)/(k1-k2);
+ koord[1]=(k1*b2-k2*b1)/(k1-k2);
+return koord;  
+}
+
+if (k1==k2)
+Console.WriteLine("Прямые параллельны и не имеют точек пересечения");
+else {
+double[] a = FindPoint(b1, k1, b2, k2);
+
+Console.Write("Точка пересечения двух прямых");
+Console.Write(" Координата Х:");
+Console.Write(a[0]);
+Console.Write(" Координата Y:");
+Console.Write(a[1]);
+}
